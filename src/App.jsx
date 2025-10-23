b5449e62ebdd9ffe1772414271aeaef8be2855ca
1967cooder <silvanalindholm@hotmail.com>
2025-10-23 19:34:53 +0300

    added componennts, App.jsx construction,changes to App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
+import Card from "./Components/Card.jsx";
+import Header from "./Components/Header.jsx";
+import Footer from "./Components/Footer.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Card
+          name="Amy"
+          title="Software Engineer"
+          salary="€120,000"
+          phone="555-1234"
+          email="amy@hotmail.com"
+          animal="Cat"
+        />
+        <Card
+          name="Bita"
+          title="Product Manager"
+          salary="€130,000"
+          phone="555-5678"
+          email="bita@hotmail.com"
+          animal="Owl"
+        />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = (props) => {
+  return <h3 className="footer">© 2024 hrApp. All rights reserved.</h3>;
+};
+
+export default Footer;

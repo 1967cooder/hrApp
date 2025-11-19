5f2e8015dfe15d6c901b9f660ce60860331d1101
1967cooder <silvanalindholm@hotmail.com>
2025-11-19 22:07:40 +0200

    Updated App.jsx with new feature/fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import * as styles from "./index.css";
+import "./index.css";
 import PersonList from "./Components/PersonList.jsx";
 import Header from "./Components/Header.jsx";
 import About from "./Components/About.jsx";
